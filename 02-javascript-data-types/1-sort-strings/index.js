d3223def6aa243d130babe8f7053b5347d0bd393
n.konohova <n.v.konokhova@gmail.com>
2021-09-28 17:17:27 +0300

    hw2 change task1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,13 +1,6 @@
 export function sortStrings(arr, param = 'asc') {
   const arrSorted = arr.slice();
-  for (let i = 0; i < arrSorted.length - 1 ; i++) {
-    for (let j = 0; j < arrSorted.length - 1 ; j++) {
-      if (arrSorted[j].localeCompare(arrSorted[j + 1], ['ru', 'en'], {sensitivity: 'variant', caseFirst: 'upper'}) > 0) {
-        const temp = arrSorted[j];
-        arrSorted[j] = arrSorted[j + 1];
-        arrSorted[j + 1] = temp;
-      }
-    }
-  }
-  return (param === 'asc')? arrSorted : arrSorted.reverse();
+  const compareParams = [['ru', 'en'], {sensitivity: 'variant', caseFirst: 'upper'}];
+  arrSorted.sort((a,b) => param === 'asc'? a.localeCompare(b, ...compareParams): -a.localeCompare(b, ...compareParams));
+  return arrSorted;
 }
