dd035cc63e0723686dc38971a020b5ee324c4492
n.konohova <n.v.konokhova@gmail.com>
2021-09-30 23:42:45 +0300

    hw3 task3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,9 +1,16 @@
-/**
- * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
- * @param {string} string - the initial string
- * @param {number} size - the allowed size of consecutive identical symbols
- * @returns {string} - the new string without extra symbols according passed size
- */
 export function trimSymbols(string, size) {
-
+  if (size === undefined) return string;
+  let sum = '';
+  let i = 0;
+  while (i < string.length) {
+      let count = 1;
+      const symbol = string[i];
+      for (let j = i + 1; j <= string.length; j++) {
+        const delta = j-i;
+        if (string.substring(i, j) === symbol.repeat(delta)) count=delta;
+      }
+      i = i + count;
+      if (count >= size) sum += symbol.repeat(size); else sum +=symbol;
+  }
+  return sum;
 }
