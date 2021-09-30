58a12c3339aa864aa0e6784c1b8bec90b86c135e
n.konohova <n.v.konokhova@gmail.com>
2021-10-01 00:28:03 +0300

    hw3 task1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,12 @@
-/**
- * createGetter - creates function getter which allows select value from object
- * @param {string} path - the strings path separated by dot
- * @returns {function} - function-getter which allow get value from object by set path
- */
 export function createGetter(path) {
-
+  const keys = path.split('.');
+  return function(obj) {
+    let index = 0;
+    let tmp = {...obj};
+    while (typeof tmp[keys[index]] === 'object') {
+      tmp = tmp[keys[index]];
+      index ++;
+    }
+    return tmp[keys[index]];
+  }
 }
