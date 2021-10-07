03b7f15f945e33bbbe82229ef5bc090cb85f7c4b
n.konohova <n.v.konokhova@gmail.com>
2021-10-07 23:29:18 +0300

    hw5 task1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,45 @@
 export default class NotificationMessage {
+  element;
+  toSeconds = (value) => value/1000;
 
+  constructor(message = '', {duration = 0, type='success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.createTemplate();
+    this.element = wrapper.firstElementChild;
+  }
+
+  show(target) {
+    if (target) {
+      target.insertAdjacentElement('afterend', this.element);
+      this.element = target;
+    } else this.element.replaceWith(this.element);
+    setTimeout(() => {this.remove()}, this.duration)
+  }
+
+  createTemplate() {
+    return `<div class="notification ${this.type}" style="--value:${this.toSeconds(this.duration)}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>`
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
